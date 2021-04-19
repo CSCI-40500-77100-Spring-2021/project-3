0caad1b6e8edd4a4bba9ed7c06edf00e5f61fd4a
alleene-lacaba <alleene.lacaba16@myhunter.cuny.edu>
2021-04-19 08:54:44 -0400

    added routes folder, models, users.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { AppRegistry, View, Image, StyleSheet } from 'react-native';
 import './App.css';
+import Outliner from './components/outliner';
+import Feed from './components/feed';
+import Recommendations from './components/recommendations';
+import TreeView from './components/treeview';
+import AddTrackView from './components/addtrackview';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props)  {
+    super(props);
+    this.state = { 
+      showTreeView: false,
+      showAddTrackView: false
+    };
+  }
+    
+  toggleTreeView() {
+      this.setState({
+          showTreeView: !this.state.showTreeView,
+          showAddTrackView: false
+      });
+  }
+
+  toggleAddTrackView() {
+      this.setState({
+          showTreeView: false,
+          showAddTrackView: !this.showAddTrackView
+      });
+  }
+
+  render () {
+    return (
+      <div className="App">
+          <View style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }} className="page">
+            <Outliner></Outliner>
+
+            <Feed 
+            toggleTreeView={this.toggleTreeView.bind(this)}
+            toggleAddTrackView={this.toggleAddTrackView.bind(this)}
+            ></Feed>
+
+            <Recommendations></Recommendations>
+            
+          </View>
+          <View style={{position:'absolute', top:0}}>
+            {this.state.showTreeView ?
+                <TreeView 
+                style={{zIndex:2}}
+                toggleTreeView={this.toggleTreeView.bind(this)}
+                />
+                : null
+            }
+          </View>
+          <View style={{position:'absolute', top:0}}>
+            {this.state.showAddTrackView ?
+                <AddTrackView 
+                style={{zIndex:2}}
+                toggleAddTrackView={this.toggleAddTrackView.bind(this)}
+                />
+                : null
+            }
+          </View>
+      </div>
+    );
+  }
 }
 
 export default App;
