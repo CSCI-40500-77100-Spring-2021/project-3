7641dbd718e745e893bc007bc5ffd916c4cd8ce0
alleene-lacaba <alleene.lacaba16@myhunter.cuny.edu>
2021-04-23 10:58:21 -0400

    styled add media form

--- frontend/src/components/addmedia.component.js
+++ frontend/src/components/addmedia.component.js
@@ -8,12 +8,13 @@ export default class  extends Component {
     return ( 
         <div id="add-media-container">
             <h2>Add a new audio track</h2>
-            <form action="/action_page.php">
-                <label for="audio">Choose audio:</label>
-                <input type="file" id="audio" name="audio" accept="audio/*,video/*,"/>
-                <label for="record">Choose audio:</label>
-                <input type="file" id="record" name="record" accept="audio/*,video/*,"/>
-                <button name="submit">Add audio</button>
+            <form action="">
+                <label for="audio">Choose audio:</label><br/>
+                <input type="file" id="audio" name="audio" accept="audio/*,video/*,"/><br/>
+                <br/><p>Or Record Track</p><br/>
+                <label for="record">Choose audio:</label><br/>
+                <input type="file" id="record" name="record" accept="audio/*,video/*,"/><br/>
+                <button id="submit-button" name="submit">Add audio</button>
             </form>
         </div>
         
